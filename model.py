29dd2680b82120eb86f2e20ad88d7e6f0d06e5ee
NidhiPandey18 <pandeynidhi0501@gmail.com>
2026-04-14 17:16:59 +0530

    Implement image detection based on brightness
    Add image detection function to classify images based on brightness.

--- model.py
+++ model.py
@@ -1,1 +1,14 @@
+from PIL import Image, ImageStat
+import numpy as np
 
+def detect_image(path):
+  image = image.open(path)
+  arr = np.array(image)
+  mean = arr.mean()
+
+if mean < 120:
+  return "✅Real"
+elif mean < 180:
+  return "⚠️Suspicious"
+else:
+  return "❌AI Generated"
